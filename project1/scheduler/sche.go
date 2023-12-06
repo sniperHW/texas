e8a718e9195623add12933f6c38d21c42b5f9916
sniperHW <sniperHW@163.com>
2023-12-06 23:53:19 +0800

    none

--- project1/scheduler/sche.go
+++ project1/scheduler/sche.go
@@ -179,6 +179,7 @@ type sche struct {
 	taskFailedRecord  *os.File
 	c                 chan string
 	loadCounter       int
+	averageTaskMem    int64
 }
 
 func isFileExist(path string, compress bool) bool {
@@ -248,6 +249,8 @@ func (g *taskGroup) loadTaskFromFile(s *sche) error {
 		}
 
 		t.MemNeed += s.cfg.MemoryRevise[m]
+
+		s.averageTaskMem += int64(t.MemNeed)
 
 		if err != nil {
 			err = fmt.Errorf("(2)invaild task:%s error:%v", lineStr, err)
@@ -429,6 +432,8 @@ func (s *sche) init() error {
 	sort.Slice(s.unAllocTasks, func(i, j int) bool {
 		return s.unAllocTasks[i].less(s.unAllocTasks[j])
 	})
+
+	s.averageTaskMem /= int64(len(s.tasks))
 
 	//监控s.cfg.TaskCfg
 	watch, _ := fsnotify.NewWatcher()
@@ -822,13 +827,10 @@ func (s *sche) stop() {
 func (s *sche) onWorkerAvaliable(w *worker, dosort bool) {
 	if len(w.tasks) < MaxTaskCount && atomic.LoadInt32(&s.dispatchFlag) == 1 && atomic.LoadInt32(&s.pauseFlag) == 0 {
 		last := len(s.unAllocTasks) - 1
-		if last >= 0 && w.memory >= s.unAllocTasks[last].MemNeed {
-			taskIdx := []int{}
-			//todo:通过二分查找优化
-			for i := 0; i < len(s.unAllocTasks) && len(w.tasks) < MaxTaskCount; i++ {
-				v := s.unAllocTasks[i]
-				if w.memory >= v.MemNeed {
-					taskIdx = append(taskIdx, i)
+		if last >= 0 {
+			if s.unAllocTasks[last].MemNeed < 1 {
+				for len(w.tasks) < MaxTaskCount && last >= 0 {
+					v := s.unAllocTasks[last]
 					logger.Sugar().Debugf("dispatchJob %s to worker:%s mem before:%d mem after:%d task mem:%d", v.Id, w.workerID, w.memory, w.memory-v.MemNeed, v.MemNeed)
 					w.memory -= v.MemNeed
 					w.tasks[v.Id] = v
@@ -837,19 +839,38 @@ func (s *sche) onWorkerAvaliable(w *worker, dosort bool) {
 					v.deadline = time.Now().Add(time.Second * taskTimeout)
 					s.doing[v.Id] = v
 					w.dispatchJob(v, s.cfg.ThreadReserved)
+					s.unAllocTasks = s.unAllocTasks[:len(s.unAllocTasks)-1]
+					last = len(s.unAllocTasks) - 1
 				}
-			}
+			} else if w.memory >= s.unAllocTasks[last].MemNeed {
+				taskIdx := []int{}
+				//todo:通过二分查找优化
+				for i := 0; i < len(s.unAllocTasks) && len(w.tasks) < MaxTaskCount; i++ {
+					v := s.unAllocTasks[i]
+					if w.memory >= v.MemNeed {
+						taskIdx = append(taskIdx, i)
+						logger.Sugar().Debugf("dispatchJob %s to worker:%s mem before:%d mem after:%d task mem:%d", v.Id, w.workerID, w.memory, w.memory-v.MemNeed, v.MemNeed)
+						w.memory -= v.MemNeed
+						w.tasks[v.Id] = v
+						v.WorkerID = w.workerID
+						v.save(s.db)
+						v.deadline = time.Now().Add(time.Second * taskTimeout)
+						s.doing[v.Id] = v
+						w.dispatchJob(v, s.cfg.ThreadReserved)
+					}
+				}
 
-			if len(taskIdx) > 0 {
-				c := len(s.unAllocTasks) - 1
-				for _, v := range taskIdx {
-					s.unAllocTasks[c], s.unAllocTasks[v] = s.unAllocTasks[v], s.unAllocTasks[c]
-					c--
+				if len(taskIdx) > 0 {
+					c := len(s.unAllocTasks) - 1
+					for _, v := range taskIdx {
+						s.unAllocTasks[c], s.unAllocTasks[v] = s.unAllocTasks[v], s.unAllocTasks[c]
+						c--
+					}
+					s.unAllocTasks = s.unAllocTasks[:len(s.unAllocTasks)-len(taskIdx)]
+					sort.Slice(s.unAllocTasks, func(i, j int) bool {
+						return s.unAllocTasks[i].less(s.unAllocTasks[j])
+					})
 				}
-				s.unAllocTasks = s.unAllocTasks[:len(s.unAllocTasks)-len(taskIdx)]
-				sort.Slice(s.unAllocTasks, func(i, j int) bool {
-					return s.unAllocTasks[i].less(s.unAllocTasks[j])
-				})
 			}
 		}
 	}
